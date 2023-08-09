b19ea5c9e7398f6b157e149b0ba77366e5b9233c
ViktoriyaSF <kievstar01051985@gmail.com>
2023-08-09 17:46:51 +0300

    creating a scheme and model

---
+++ routes/reviews.js
@@ -0,0 +1,8 @@
+const express = require("express");
+// const {
+//   reviewsAddSchema,
+//   reviewsUpdateSchema,
+// } = require("../schemas/reviewSchemas");
+const router = express.Router();
+
+module.exports = router;
