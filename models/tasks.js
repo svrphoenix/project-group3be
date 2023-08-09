4e7945a88fc452b77db9bc26b8ec89ac3469783e
KuhaiAnna <kuhai.anna@gmail.com>
2023-08-09 23:16:15 +0300

    Create task model

---
+++ models/tasks.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const { mongooseErrorHandler } = require("../helpers");
+
+const taskSchema = new Schema(
+  {
+    title: { type: String, required: [true, "Enter a task title"] },
+    start: { type: String, required: [true, "Enter the task start time"] },
+    end: { type: String, required: [true, "Enter the task end time"] },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      required: [true, "Select a priority for the task"],
+    },
+    date: {
+      type: String,
+      required: [true, "Add a date for the task"],
+    },
+    category: {
+      type: String,
+      enum: ["to-do", "in-progress", "done"],
+      required: [true, "Select a task category"],
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      require: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+taskSchema.post("save", mongooseErrorHandler);
+
+const Task = model("task", taskSchema);
+
+module.exports = { Task };
