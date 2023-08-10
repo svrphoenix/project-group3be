1f406b80200c8b6df58188175687604c74112181
KuhaiAnna <kuhai.anna@gmail.com>
2023-08-10 22:01:23 +0300

    Add taskRouter to app.js

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerDoc = require("./swagger.json");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 
-const reviewsRouter = require("./routes/reviews");
+const reviewsRouter = require('./routes/reviews');
+const taskRouter = require('./routes/tasks');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-app.use("/reviews", reviewsRouter);
+app.use('/reviews', reviewsRouter);
+app.use('/tasks', taskRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Path not found" });
+  res.status(404).json({ message: 'Path not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = "Internal server error" } = err;
+  const { statusCode = 500, message = 'Internal server error' } = err;
   res.status(statusCode).json({ message });
 });
 
