88117ea0c1e6bcca6ad290cd6c7caea032f109d5
ViktoriyaSF <kievstar01051985@gmail.com>
2023-08-10 21:03:11 +0300

    getAllReviews and getUserReview

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -6,10 +6,27 @@ const getAllReviews = ctrlWrapper(async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 2 } = req.query;
   const skip = (page - 1) * limit;
-  const reviews = await Review.find({}, '-createdAt', '-updatedAt', {
+  const reviews = await Review.find({}, '-createdAt -updatedAt', {
     skip,
     limit,
     sort: { createdAt: -1 },
   }).populate('owner', 'name avatarURL');
   res.status(200).json(reviews);
 });
+
+const getUserReview = ctrlWrapper(async (req, res) => {
+  const { _id: owner } = req.user;
+  const result = await Review.find({ owner }).populate(
+    'owner',
+    'name avatarURL'
+  );
+  if (!result) {
+    throw new HttpError(404, 'Not found.🤷‍♀️');
+  }
+  return result;
+});
+
+module.exports = {
+  getAllReviews,
+  getUserReview,
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,16 @@
-const express = require("express");
+const express = require('express');
 // const {
 //   reviewsAddSchema,
 //   reviewsUpdateSchema,
 // } = require("../helpers/validation/reviewSchemas");
+const {
+  getAllReviews,
+  getUserReview,
+} = require('../controllers/reviewsController');
 const router = express.Router();
+
+router.route('/').get(getAllReviews);
+
+router.route('/:id').get(getUserReview);
 
 module.exports = router;
