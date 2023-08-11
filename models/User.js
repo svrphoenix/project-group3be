1fd147339163743c35cc39f0d3b1c900cff2f46c
Serhii Rudenko <svr.phoenix@gmail.com>
2023-08-11 09:51:42 +0300

    Updated User model - avatarURL

--- models/User.js
+++ models/User.js
@@ -21,7 +21,8 @@ const userSchema = new Schema(
     },
     avatarURL: {
       type: String,
-      required: true,
+      required: false,
+      default: '',
     },
     refresh_token: String,
   },
