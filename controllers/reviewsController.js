9dd1b27b4e16c6f5b61a3674a2983bd365867e5e
ViktoriyaSF <kievstar01051985@gmail.com>
2023-08-11 18:40:34 +0300

    vlidate_review

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -3,7 +3,6 @@ const HttpError = require('../helpers/HttpError');
 const ctrlWrapper = require('../helpers/ctrlWrapper');
 
 const getAllReviews = ctrlWrapper(async (req, res) => {
-  const { _id: owner } = req.user;
   const { page = 1, limit = 2 } = req.query;
   const skip = (page - 1) * limit;
   const reviews = await Review.find({}, '-createdAt -updatedAt', {
