4c167e17f5dc4d649768a8c565fb044ba8179781
Serhii Rudenko <svr.phoenix@gmail.com>
2023-08-12 08:20:27 +0300

    Updated refreshToken check and update

--- helpers/validation/authValidationSchema.js
+++ helpers/validation/authValidationSchema.js
@@ -1,8 +1,8 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
-const {emailRegex, passwordRegex } = require('./const');
+const { emailRegex, passwordRegex } = require('./const');
 
-const registerValidationSchema=Joi.object({
+const registerValidationSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().pattern(emailRegex).required(),
   password: Joi.string().pattern(passwordRegex).required().messages({
@@ -11,4 +11,20 @@ const registerValidationSchema=Joi.object({
   }),
 });
 
-module.exports = { registerValidationSchema };
+const loginValidationSchema = Joi.object({
+  email: Joi.string().pattern(emailRegex).required(),
+  password: Joi.string().pattern(passwordRegex).required().messages({
+    'string.pattern.base':
+      'This password should contain  at least eight characters and at least one number and one letter',
+  }),
+});
+
+const refreshSchema = Joi.object({
+  refresh_token: Joi.string().required(),
+});
+
+module.exports = {
+  registerValidationSchema,
+  refreshSchema,
+  loginValidationSchema,
+};
