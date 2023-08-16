a841723c2d9b2e8d67e157fd913d01ce9807ca7d
ViktoriyaSF <kievstar01051985@gmail.com>
2023-08-16 22:34:21 +0300

    cleared the parameters

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -3,11 +3,7 @@ const HttpError = require('../helpers/HttpError');
 const ctrlWrapper = require('../helpers/ctrlWrapper');
 
 const getAllReviews = ctrlWrapper(async (req, res) => {
-  const { page = 1, limit = 2 } = req.query;
-  const skip = (page - 1) * limit;
   const reviews = await Review.find({}, '-createdAt -updatedAt', {
-    skip,
-    limit,
     sort: { createdAt: -1 },
   }).populate('owner', 'name avatarURL');
   res.status(200).json(reviews);
