ff2eb9bf6149e1bbf394692afdf472279a846da0
Serhii Rudenko <svr.phoenix@gmail.com>
2023-08-19 02:23:01 +0300

    Added new files

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const authRouter = require('./auth');
+const reviewsRouter = require('./reviews');
+const taskRouter = require('./tasks');
+
+module.exports = { authRouter, reviewsRouter, taskRouter };
