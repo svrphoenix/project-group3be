c14a7615ec6ac73cb999042f26b8e0bed5c15961
Serhii Rudenko <svr.phoenix@gmail.com>
2023-08-20 00:26:08 +0300

    Changes in validation schemas

--- helpers/validation/authValidationSchema.js
+++ helpers/validation/authValidationSchema.js
@@ -4,10 +4,12 @@ const { emailRegex, passwordRegex } = require('../../const');
 
 const registerValidationSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().pattern(emailRegex).required(),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    'string.pattern.base': 'Invalid email format',
+  }),
   password: Joi.string().pattern(passwordRegex).required().messages({
     'string.pattern.base':
-      'This password should contain  at least eight characters and at least one number and one letter',
+      'Password should contain  at least eight characters and at least one number and one letter',
   }),
 });
 
--- helpers/validation/updateUserSchema.js
+++ helpers/validation/updateUserSchema.js
@@ -5,10 +5,18 @@ const { registerValidationSchema } = require('./authValidationSchema');
 const updateUserSchema = Joi.object().keys({
   name: registerValidationSchema.extract('name'),
   email: registerValidationSchema.extract('email'),
-  phone: Joi.string().pattern(phoneRegex),
-  skype: Joi.string().pattern(skypeRegex),
-  birthday: Joi.string().pattern(birthdayRegex),
-  avatarURL: Joi.string().allow(''),
+  phone: Joi.string().pattern(phoneRegex).allow('').messages({
+    'string.pattern.base':
+      'Valid number is +38(000)1234567 or 38(000)1234567 and country code consist of 1-3 numbers or empty field phone',
+  }),
+  skype: Joi.string().pattern(skypeRegex).allow('').messages({
+    'string.pattern.base':
+      'Valid skype number is +38(000)1234567 or 38(000)1234567 and country code consist of 1-3 numbers or empty field skype',
+  }),
+  birthday: Joi.string().pattern(birthdayRegex).messages({
+    'string.pattern.base': 'Valid birthday has format YYYY-MM-DD',
+  }),
+  avatarURL: Joi.string(),
 });
 
 module.exports = { updateUserSchema };
