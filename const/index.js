63453f30c51379a3b385bdf20267677f51d94952
Serhii Rudenko <svr.phoenix@gmail.com>
2023-08-22 23:51:08 +0300

    fixing regex email

--- const/index.js
+++ const/index.js
@@ -1,4 +1,4 @@
-const emailRegex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+const emailRegex = /^[a-z0-9.]+@[a-z]+\.[a-z]{2,3}$/;
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,30}$/;
 const phoneRegex = /^(\+\d{1,3}|\d{1,3}) \(\d{3}\) \d{3} \d{2} \d{2}$/;
 const skypeRegex = /^(\+\d{1,3}|\d{1,3}) \(\d{3}\) \d{3} \d{2} \d{2}$/;
